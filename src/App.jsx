f382cfa09b271988d02e1731a55115a49afa73dc
saelu <40205419+saelu@users.noreply.github.com>
2025-07-05 19:50:42 +0100

    add final content

--- src/App.jsx
+++ src/App.jsx
@@ -27,10 +27,10 @@ const App = () => {
     <div>
       <Navigation />
       <Header data={landingPageData.Header} />
-      <Features data={landingPageData.Features} />
       <About data={landingPageData.About} />
       <Services data={landingPageData.Services} />
       {/* <Gallery data={landingPageData.Gallery} /> */}
+      <Features data={landingPageData.Features} />
       <Testimonials data={landingPageData.Testimonials} />
       {/* <Team data={landingPageData.Team} /> */}
       <Contact data={landingPageData.Contact} />
