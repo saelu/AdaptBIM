61b7653a97eb7e9e95161b7f4e935dcf4d7f9c57
saelu <40205419+saelu@users.noreply.github.com>
2025-07-05 19:02:47 +0100

    add final content

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -132,7 +132,7 @@ export const Contact = (props) => {
               </p>
             </div>
           </div>
-          <div className="col-md-12">
+          {/* <div className="col-md-12">
             <div className="row">
               <div className="social">
                 <ul>
@@ -154,7 +154,7 @@ export const Contact = (props) => {
                 </ul>
               </div>
             </div>
-          </div>
+          </div> */}
         </div>
       </div>
       <div id="footer">
