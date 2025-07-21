a1d70adb9a6907ad455dad3e1903e6a1c379e727
saelu <40205419+saelu@users.noreply.github.com>
2025-07-21 04:00:26 +0100

    perfonmance issue fix 8

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,34 +4,10 @@ export const Header = (props) => {
   return (
     <header id="header">
       <div className="intro">
-        {/* LCP Image - Hidden but discoverable */}
-        <picture>
-          <source 
-            srcSet="/img/webp/intro-bgd.webp" 
-            type="image/webp"
-          />
-          <img 
-            src="/img/optimized/intro-bgd.png"
-            alt="AdaptBIM Hero Background"
-            style={{
-              position: 'absolute',
-              top: 0,
-              left: 0,
-              width: '100%',
-              height: '100%',
-              objectFit: 'cover',
-              zIndex: -1,
-              opacity: 0
-            }}
-            fetchPriority="high"
-            loading="eager"
-          />
-        </picture>
-        
         <div className="overlay">
           <div className="container">
             <div className="row">
-              <div className="col-md-8 col-md-offset-2 intro-text">
+              <div className="col-md-8 col-md-offset-2 col-sm-10 col-sm-offset-1 col-xs-12 intro-text">
                 <h1>
                   Empowering <span className="aec">AEC</span> and <span  className="aec">EPC</span> with our Tailored 
                   <span className="aec"> BIM</span> Solutions
